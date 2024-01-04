974a7c488871b1b4ab22750e2d869020c28aced1
sgoodyear <96261962+sgoodyear@users.noreply.github.com>
2024-01-03 21:15:12 -0700

    Update Fan_RPM_Control.ino

--- Arduino Programs/Fan_RPM_Control.ino
+++ Arduino Programs/Fan_RPM_Control.ino
@@ -1,5 +1,10 @@
-const int s1fan = 4;  // Attach GREEN to "D4"
-const int s2fan = 5;  // Attach YELLOW to "D5"
+// DO NOT CONNECT THE ESP32 TO 12V!
+
+// Attach simulation rig's 5v to to pin VUSB
+// Connect the fan's ground wires (BLACK and BROWN) to Ground
+
+const int frontfan = 4;  // Attach GREEN to "D4"
+const int rearfan = 5;   // Attach YELLOW to "D5"
 
 const int potPin1 = A2;  // Potentiometer 1 to pin "D2"
 const int potPin2 = A3;  // Potentiometer 2 to pin "D3"
@@ -13,8 +18,8 @@ void setup() {
   ledc2.setup(2, 25000, 8);  // channel 2, 25000 Hz, 8-bit resolution
 
   // Attach PWM channels to fan speed control pins
-  ledc1.attachPin(s1fan);
-  ledc2.attachPin(s2fan);
+  ledc1.attachPin(frontfan);
+  ledc2.attachPin(rearfan);
 
   // Set initial fan speed to zero
   ledc1.write(0);
@@ -27,12 +32,12 @@ void loop() {
   int potValue2 = analogRead(potPin2);
 
   // Remap potentiometer input signal range to PWM ouput signal range
-  int s1rpm = map(potValue1, 0, 1023, 0, 255);
-  int s2rpm = map(potValue2, 0, 1023, 0, 255);
+  int frontfanrpm = map(potValue1, 0, 1023, 0, 255);
+  int rearfanrpm = map(potValue2, 0, 1023, 0, 255);
 
   // Update fan speed
-  ledc1.write(s1rpm);
-  ledc2.write(s2rpm);
+  ledc1.write(frontfanrpm);
+  ledc2.write(rearfanrpm);
 
   delay(10);
 }
