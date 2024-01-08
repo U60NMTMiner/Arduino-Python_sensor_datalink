0319660178d42a9ab013776e066f1350901e63f4
sgoodyear <96261962+sgoodyear@users.noreply.github.com>
2024-01-08 15:18:07 -0700

    Update Fan_RPM_Control.ino
    Tested and working!

--- Arduino Programs/Fan_RPM_Control.ino
+++ Arduino Programs/Fan_RPM_Control.ino
@@ -3,41 +3,57 @@
 // Attach simulation rig's 5v to to pin VUSB
 // Connect the fan's ground wires (BLACK and BROWN) to Ground
 
-const int frontfan = 4;  // Attach GREEN to "D4"
-const int rearfan = 5;   // Attach YELLOW to "D5"
+
+const int frontfan = D4;  // Attach GREEN to "D4"
+const int rearfan = D6;   // Attach YELLOW to "D6"
 
 const int potPin1 = A2;  // Potentiometer 1 to pin "D2"
 const int potPin2 = A3;  // Potentiometer 2 to pin "D3"
 
-LEDC ledc1;
-LEDC ledc2;
+
 
 void setup() {
+  Serial.begin(115200);
+
   // Set up PWM channels
-  ledc1.setup(1, 25000, 8);  // channel 1, 25000 Hz, 8-bit resolution
-  ledc2.setup(2, 25000, 8);  // channel 2, 25000 Hz, 8-bit resolution
+  ledcSetup(1, 25000, 8);  // channel 1, 25000 Hz, 8-bit resolution
+  ledcSetup(2, 25000, 8);  // channel 2, 25000 Hz, 8-bit resolution
 
   // Attach PWM channels to fan speed control pins
-  ledc1.attachPin(frontfan);
-  ledc2.attachPin(rearfan);
+  ledcAttachPin(frontfan, 1);
+  ledcAttachPin(rearfan, 2);
 
   // Set initial fan speed to zero
-  ledc1.write(0);
-  ledc2.write(0);
+  ledcWrite(frontfan, 0);
+  ledcWrite(rearfan, 0);
+
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on
+
   // Read potentiometers
   int potValue1 = analogRead(potPin1);
   int potValue2 = analogRead(potPin2);
 
   // Remap potentiometer input signal range to PWM ouput signal range
-  int frontfanrpm = map(potValue1, 0, 1023, 0, 255);
-  int rearfanrpm = map(potValue2, 0, 1023, 0, 255);
+  int frontfanrpm = map(potValue2, 0, 4095, 255, 5);
+  int rearfanrpm = map(potValue1, 0, 4095, 255, 5);
+
+  Serial.print(potValue1);
+  Serial.print(" ");
+  Serial.println(potValue2);
+
+  Serial.print(frontfanrpm);
+  Serial.print(" ");
+  Serial.println(rearfanrpm);
 
   // Update fan speed
-  ledc1.write(frontfanrpm);
-  ledc2.write(rearfanrpm);
+  ledcWrite(1, frontfanrpm);
+  ledcWrite(2, rearfanrpm);
 
   delay(10);
 }
